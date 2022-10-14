e373ed8536124b9591569afffbc51b46c670c169
manuel88manu <92389779+manuel88manu@users.noreply.github.com>
2022-10-14 13:36:58 -0600

    React PWA completo

---
+++ src/components/Principal.tsx
@@ -0,0 +1,18 @@
+export const Principal=()=>{
+    return(
+        <div>
+            <h3>Principal</h3>
+            <button className="btn btn-primary">Bienvenido</button>
+            <br />
+            <img src="../images/guitarra.jpg"
+             alt="guitar"
+             style={{
+                width:200,
+                height:150
+             }} 
+             
+             />
+             <h3>Test Cambio en PWA con exito</h3>
+        </div>
+    )
+}
